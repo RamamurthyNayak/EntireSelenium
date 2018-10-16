de4143c8710d2d5afc0c537cf754e70139bf4590
Admin <Admin@192.168.0.2>
2018-10-16 18:42:05 +0530

    uploading

---
+++ Automation1/src/JSP/DummyProject.java
@@ -0,0 +1,16 @@
+package JSP;
+
+public class DummyProject {
+
+	static int age=20;
+	static String name="Rama";
+	
+	
+	public static void main(String[] args) {
+		DummyProject dp=new DummyProject();
+	
+		System.out.println("Hi how are you? "+"  "+name+" aged  "+age);
+
+	}
+
+}
